c5b7fd5d7578044c4e5aceb3683422a9a55536df
pochi-pochi <takahiroa500@outlook.jp>
2024-05-01 09:19:06 +0900

    [add]bashの指定
    bashを指定してコードを実行するように変更しました。

--- start_flask_app.sh
+++ start_flask_app.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 cd /path/to/your/app
 source ./venv/bin/activate
 # gitから最新のコードをプル
 git pull origin master
+pip install -r requirements.txt
 flask run &
+sleep 3
 xdg-open http://127.0.0.1:5000
